SeelabFhdo/lemma

commit 1855d63f34fbf6fadaf9b9f9e0c8b04bad9c01b7

Service and Technology DSL: Don't use synchronous as default value for communication types

de.fhdo.ddmm.servicedsl/src/de/fhdo/ddmm/scoping/ServiceDslScopeProvider.xtend
@@ -626,10 +626,12 @@ class ServiceDslScopeProvider extends AbstractServiceDslScopeProvider {
         val isProtocolSpecification = importedProtocol.specification !== null
         val CommunicationType filterCommunicationType = if (isProtocolSpecification)
             importedProtocol.specification.communicationType
+        if (filterCommunicationType === null)
+            return IScope.NULLSCOPE
 
         val resourceRoot = resourceContents.get(0) as Technology
         val scopeElements = resourceRoot.protocols
-            .filter[!isProtocolSpecification || communicationType == filterCommunicationType]
+            .filter[!isProtocolSpecification || filterCommunicationType == communicationType]
             .map[
                 val protocolName = QualifiedName.create(it.qualifiedNameParts)
                 EObjectDescription.create(protocolName, it)

ServiceViewpointModel/model/ServiceViewpointModel.xcore
@@ -571,7 +571,7 @@ class Parameter {
 
     String name
     ExchangePattern exchangePattern = "IN"
-    CommunicationType communicationType = "SYNCHRONOUS"
+    CommunicationType communicationType
     boolean communicatesFault = "false"
     boolean optional = "false"
 
@@ -811,7 +811,7 @@ class ImportedProtocolAndDataFormat {
  * operations.
  */
 class ProtocolSpecification {
-    CommunicationType communicationType = "SYNCHRONOUS"
+    CommunicationType communicationType
 
     contains ImportedProtocolAndDataFormat protocol opposite specification
 }

TechnologyDefinitionModel/model/TechnologyDefinitionModel.xcore
@@ -84,7 +84,7 @@ class Protocol {
      */
 
     String name
-    CommunicationType communicationType = "SYNCHRONOUS"
+    CommunicationType communicationType
     boolean ^default = "false"
 
     refers DataFormat defaultFormat

de.fhdo.ddmm.servicedsl/src/de/fhdo/ddmm/ServiceDsl.xtext
@@ -85,7 +85,7 @@ ReferredOperation returns ReferredOperation:
 ;
 
 Parameter returns Parameter:
-    communicationType=CommunicationType?
+    communicationType=CommunicationType
     exchangePattern=ExchangePattern?
     (communicatesFault?='fault')?
     name=ID

de.fhdo.ddmm.servicedsl/src/de/fhdo/ddmm/validation/ServiceDslValidator.xtend
@@ -270,7 +270,7 @@ class ServiceDslValidator extends AbstractServiceDslValidator {
                 case ASYNCHRONOUS: "asynchronous"
             }
 
-            val protocolsOfType = protocols.filter[it.communicationType == communicationType]
+            val protocolsOfType = protocols.filter[communicationType == it.communicationType]
             if (protocolsOfType.size > 1)
                 error('''There must not be more than one «protocolTypeName» protocol for the ''' +
                     '''«containerName»''', protocolsOfType.get(1),

de.fhdo.ddmm.technology.technologydsl/src/de/fhdo/ddmm/technology/TechnologyDsl.xtext
@@ -40,7 +40,7 @@ TechnologyImport returns TechnologyImport:
 ;
 
 Protocol returns Protocol:
-    communicationType=CommunicationType?
+    communicationType=CommunicationType
     name=ID
     'data' 'formats' dataFormats+=DataFormat (',' dataFormats+=DataFormat)*
     (default?='default' 'with' 'format' defaultFormat=[DataFormat])?

de.fhdo.ddmm.technology.technologydsl/src/de/fhdo/ddmm/technology/validation/TechnologyDslValidator.xtend
@@ -255,7 +255,7 @@ class TechnologyDslValidator extends AbstractTechnologyDslValidator {
         }
 
         val definedDefaultProtocols = technology.protocols
-            .filter[^default && communicationType == forCommunicationType]
+            .filter[^default && forCommunicationType == communicationType]
 
         if (definedDefaultProtocols.empty)
             error('''Technology must define at least one default «communicationTypeString» ''' +

commit 77451776b23cd5f9a5c15feec51eab49910de627

Service DSL: Fix bug in Endpoint scoping introduced in commit 1855d63f

de.fhdo.ddmm.servicedsl/src/de/fhdo/ddmm/scoping/ServiceDslScopeProvider.xtend
@@ -626,8 +626,6 @@ class ServiceDslScopeProvider extends AbstractServiceDslScopeProvider {
         val isProtocolSpecification = importedProtocol.specification !== null
         val CommunicationType filterCommunicationType = if (isProtocolSpecification)
             importedProtocol.specification.communicationType
-        if (filterCommunicationType === null)
-            return IScope.NULLSCOPE
 
         val resourceRoot = resourceContents.get(0) as Technology
         val scopeElements = resourceRoot.protocols