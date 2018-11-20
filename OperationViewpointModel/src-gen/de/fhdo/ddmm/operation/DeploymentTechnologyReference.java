6dbb3c6b63adfd6f9f77847b6d96b7ae693efacb
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2018-11-20 21:49:43 +0100

    Operation DSL: Add missing javadoc to metamodel

--- OperationViewpointModel/src-gen/de/fhdo/ddmm/operation/DeploymentTechnologyReference.java
+++ OperationViewpointModel/src-gen/de/fhdo/ddmm/operation/DeploymentTechnologyReference.java
@@ -13,6 +13,11 @@ import org.eclipse.emf.ecore.EObject;
  * A representation of the model object '<em><b>Deployment Technology Reference</b></em>'.
  * <!-- end-user-doc -->
  *
+ * <!-- begin-model-doc -->
+ * *
+ * Reference to a deployment technology
+ * <!-- end-model-doc -->
+ *
  * <p>
  * The following features are supported:
  * </p>
--- OperationViewpointModel/src-gen/de/fhdo/ddmm/operation/InfrastructureTechnologyReference.java
+++ OperationViewpointModel/src-gen/de/fhdo/ddmm/operation/InfrastructureTechnologyReference.java
@@ -13,6 +13,11 @@ import org.eclipse.emf.ecore.EObject;
  * A representation of the model object '<em><b>Infrastructure Technology Reference</b></em>'.
  * <!-- end-user-doc -->
  *
+ * <!-- begin-model-doc -->
+ * *
+ * Reference to an infrastructure technology
+ * <!-- end-model-doc -->
+ *
  * <p>
  * The following features are supported:
  * </p>
