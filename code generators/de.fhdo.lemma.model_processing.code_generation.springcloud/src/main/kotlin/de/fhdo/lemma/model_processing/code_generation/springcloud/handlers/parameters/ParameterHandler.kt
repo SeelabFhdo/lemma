f767d75da42521db269f8c3c868b2d70f6b5460b
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2019-10-29 20:47:14 +0100

    Java base generator: Add support for generating Genlet-specific types of operation parameters

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/handlers/parameters/ParameterHandler.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/handlers/parameters/ParameterHandler.kt
@@ -9,6 +9,9 @@ import de.fhdo.lemma.model_processing.code_generation.java_base.ast.getParameter
 import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.GenletCodeGenerationHandlerI
 import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.GenletCodeGenerationHandlerResult
 import de.fhdo.lemma.model_processing.code_generation.java_base.handlers.CodeGenerationHandler
+import de.fhdo.lemma.model_processing.code_generation.java_base.languages.getTypeExpectedFromGenlet
+import de.fhdo.lemma.model_processing.code_generation.springcloud.addTypeInformationTo
+import de.fhdo.lemma.model_processing.code_generation.springcloud.languages.existsTechnologySpecificMappingForType
 import de.fhdo.lemma.service.intermediate.IntermediateParameter
 
 /**
@@ -26,12 +29,23 @@ internal class ParameterHandler : GenletCodeGenerationHandlerI<IntermediateParam
      */
     override fun execute(parameter: IntermediateParameter, method: MethodDeclaration, context: Nothing?)
         : GenletCodeGenerationHandlerResult<MethodDeclaration>? {
-        /* Currently, this handler only covers information related to API comments of parameters */
-        val apiComment = parameter.operation.apiOperationComment ?: return null
+        val generatedParameter = method.getParameter(parameter.name)!!
+
+        /* Do Genlet-specific type mapping if necessary */
+        val typeExpectedFromGenlet = generatedParameter.getTypeExpectedFromGenlet()
+        if (typeExpectedFromGenlet != null && existsTechnologySpecificMappingForType(typeExpectedFromGenlet)) {
+            val (mappedType, _) = parameter.type.addTypeInformationTo(method) {
+                addImport(it, ImportTargetElementType.METHOD)
+            }!!
+
+            generatedParameter.setType(mappedType)
+        }
+
+        /* Handle API comments of the parameter if any */
+        val apiComment = parameter.operation.apiOperationComment ?: return GenletCodeGenerationHandlerResult(method)
         val parameterComment = apiComment.parameterComments.find { it.parameter.name == parameter.name } ?: return null
 
-        /* Add imports and annotations related to API comments to parameter */
-        val generatedParameter = method.getParameter(parameter.name)!!
+        // Add imports and annotations related to API comments to parameter
         method.addImport("io.swagger.annotations.ApiParam", ImportTargetElementType.ANNOTATION,
             SerializationCharacteristic.REMOVE_ON_RELOCATION)
         val annotation = generatedParameter.addAndGetAnnotation("ApiParam",
