ce393b7871d624aa249b6e8b2fecf609b97689db
Philip Wizenty <philip.wizenty@fh-dortmund.de>
2020-02-28 09:47:17 +0100

    Fix model processing builds

--- de.fhdo.lemma.live_validation/build.sh
+++ de.fhdo.lemma.live_validation/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -z "$JAVA_HOME" && (-f set_java_home.sh) ]];
+if [ -f set_java_home.sh ];
 then
     source set_java_home.sh
-    sh set_java_home.sh && gradle fullBuild -Dorg.gradle.java.home="$JAVA_HOME"    
+    sh set_java_home.sh && ./gradlew fullBuild -Dorg.gradle.java.home="$JAVA_HOME"    
 else
-    gradle fullBuild
+    ./gradlew fullBuild 
 fi
