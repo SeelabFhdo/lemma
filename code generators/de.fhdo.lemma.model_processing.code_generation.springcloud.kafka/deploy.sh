5317f6eebc32f4f170f7bf82ebc7c5600d712147
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2020-09-22 17:48:29 +0200

    Kafka Genlet: Fix publication of Maven metadata checksums

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.kafka/deploy.sh
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.kafka/deploy.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-./gradlew -b deploy.gradle.kts publish
+# With the Gradle version used by the module it is currently required to set
+# org.gradle.internal.publish.checksums.insecure to true, because the target
+# repository does not support SHA-256 checksums for snapshot-maven-metadata.xml,
+# yet. Without the flag set to true, the
+# publishMavenPublicationToMavenRepository task will yield the following
+# warning and no checksum will be published at all.
+#       Cannot upload checksum for snapshot-maven-metadata.xml.
+#       Remote repository doesn't support sha-256.
+./gradlew -b deploy.gradle.kts \
+    -Dorg.gradle.internal.publish.checksums.insecure=true \
+    publish
