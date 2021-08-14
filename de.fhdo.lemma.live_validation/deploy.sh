f299d0d3ba5d24ce2347d48e53133b4ea22b4c85
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2021-08-14 10:11:50 +0200

    Live Validation Framework: Fix deployment

--- de.fhdo.lemma.live_validation/deploy.sh
+++ de.fhdo.lemma.live_validation/deploy.sh
@@ -3,8 +3,15 @@
 # Propagate version from root project via environment variable
 export LIVE_VALIDATION_VERSION=$(python3 ../scripts/read_version_identifier_from_build_file.py gradle.properties)
 
-./gradlew -b util/deploy.gradle.kts publish
-./gradlew -b model/deploy.gradle.kts publish
-./gradlew -b protocol/deploy.gradle.kts publish
-./gradlew -b client/deploy.gradle.kts publish
-./gradlew -b server/deploy.gradle.kts publish
+deploy_live_validation_artifact() {
+  ARTIFACT_NAME=$1
+  # Switch to sub-project directory as otherwise Gradle will search in the wrong "build/libs" directory when invoking
+  # File("build/libs") within the "deploy.gradle.kts" specifications
+  cd $ARTIFACT_NAME && ../gradlew -b deploy.gradle.kts publish && cd ..
+}
+
+deploy_live_validation_artifact "util"
+deploy_live_validation_artifact "model"
+deploy_live_validation_artifact "protocol"
+deploy_live_validation_artifact "client"
+deploy_live_validation_artifact "server"
