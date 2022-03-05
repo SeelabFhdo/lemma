a5c82df5cd105d197d7b3354b27b2ca7d4610732
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2022-03-05 11:26:29 +0100

    CQRS Genlet: Fix runtime warning concerning duplicate SLF4J bindings
        The fix removes the dependency on the fat JAR of LEMMA's model processing
        framework in order to resolve the warning.

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.cqrs/build.gradle.kts
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.cqrs/build.gradle.kts
@@ -12,23 +12,29 @@ repositories {
 
 buildscript {
     extra.set("groovyVersion", "3.0.3")
+    extra.set("jansiVersion", "1.17.1")
     extra.set("javaBaseGeneratorVersion", version)
     extra.set("javaParserVersion", "3.14.10")
     extra.set("coroutinesVersion", "1.1.1")
     extra.set("lemmaEclipsePluginsVersion", version)
     extra.set("log4jVersion", "2.16.0")
     extra.set("loggingVersion", "1.7.9")
+    extra.set("lsp4jVersion", "0.10.0")
     extra.set("modelProcessingVersion", version)
+    extra.set("picocliVersion", "3.9.3")
 }
 
 dependencies {
     val groovyVersion: String by rootProject.extra
     val javaBaseGeneratorVersion: String by rootProject.extra
     val coroutinesVersion: String by rootProject.extra
+    val jansiVersion: String by rootProject.extra
     val lemmaEclipsePluginsVersion: String by rootProject.extra
     val log4jVersion: String by rootProject.extra
     val loggingVersion: String by rootProject.extra
+    val lsp4jVersion: String by rootProject.extra
     val modelProcessingVersion: String by rootProject.extra
+    val picocliVersion: String by rootProject.extra
 
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
@@ -40,18 +46,24 @@ dependencies {
     implementation("de.fhdo.lemma.live_validation:de.fhdo.lemma.live_validation.client:$lemmaEclipsePluginsVersion")
     implementation("de.fhdo.lemma.data.datadsl:de.fhdo.lemma.data.datadsl.metamodel:$lemmaEclipsePluginsVersion")
     implementation("de.fhdo.lemma.model_processing:de.fhdo.lemma.model_processing:" +
-        "$modelProcessingVersion:all-dependencies-no-kotlin")
+        "$modelProcessingVersion")
     implementation("de.fhdo.lemma.model_processing.code_generation.java_base:" +
         "de.fhdo.lemma.model_processing.code_generation.java_base:$javaBaseGeneratorVersion")
     implementation("de.fhdo.lemma.model_processing.utils:de.fhdo.lemma.model_processing.utils:$modelProcessingVersion")
     implementation("de.fhdo.lemma.servicedsl:de.fhdo.lemma.servicedsl:$lemmaEclipsePluginsVersion")
+
+    /* Dependencies required by model processing framework */
     implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-    implementation("io.github.microutils:kotlin-logging:$loggingVersion")
     // Required by log4j (Groovy execution engine)
     implementation("org.codehaus.groovy:groovy-jsr223:$groovyVersion")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:$lsp4jVersion")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:$lsp4jVersion")
+    implementation("org.fusesource.jansi:jansi:$jansiVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("info.picocli:picocli:$picocliVersion")
+    implementation("io.github.microutils:kotlin-logging:$loggingVersion")
 }
 
 tasks {
