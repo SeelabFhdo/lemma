0a3bf5d5af597407ce9c3fe5c7a8190db0f5975b
Dominik Pelkmann <dominik@pelkmann.com>
2022-03-20 20:14:14 +0100

    wip mtls

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/mtls/Context.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/mtls/Context.kt
@@ -1,8 +1,9 @@
+package de.fhdo.lemma.model_processing.code_generation.springcloud.mtls
+
 internal object Context {
     object State {
         fun reset() {
             TODO("Not yet implemented")
         }
-
     }
 }
