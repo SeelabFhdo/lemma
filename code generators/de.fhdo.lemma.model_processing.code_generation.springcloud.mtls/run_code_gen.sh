b9374f2128e465af1d80f6cdc54b6a0715a9d2fb
Dominik Pelkmann <dominik@pelkmann.com>
2022-03-21 17:01:35 +0100

    wip

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls/run_code_gen.sh
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls/run_code_gen.sh
@@ -4,13 +4,13 @@ JAVABASE_PATH="$HOME/masterthesis/eclipse-workspace/lemma/code generators/de.fhd
 SPRINGCLOUD_PATH="$HOME/masterthesis/eclipse-workspace/lemma/code generators/de.fhdo.lemma.model_processing.code_generation.springcloud/build/libs/de.fhdo.lemma.model_processing.code_generation.springcloud-0.8.5-SNAPSHOT.jar"
 SPRINGCLOUD_MTLS_PATH="$HOME/masterthesis/eclipse-workspace/lemma/code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls/build/libs/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls-0.8.5-SNAPSHOT.jar"
 
-TARGET_PATH="$HOME/masterthesis/gen_code"
-
+TARGET_PATH="$HOME/masterthesis/lemmasecuritymodelgencode"
+MODEL_PATH="$HOME/masterthesis/runtime-EclipseApplication/lemmasecuritymodel"
 ./gradlew build
 
 echo "Java Base Generator - MS1"
 java -jar "$JAVABASE_PATH" \
-    -i "$HOME/masterthesis/runtime-EclipseApplication/lemmasecuritymodel/intermediate/service models/ms1.xmi" \
+    -i "$MODEL_PATH/intermediate/service models/ms1.xmi" \
     -t "$TARGET_PATH""/ms1" \
     --invoke_only_specified_phases \
         code_generation \
@@ -21,7 +21,7 @@ java -jar "$JAVABASE_PATH" \
 
 echo "Java Base Generator - MS2"
 java -jar "$JAVABASE_PATH" \
-    -i "$HOME/masterthesis/runtime-EclipseApplication/lemmasecuritymodel/intermediate/service models/ms2.xmi" \
+    -i "$MODEL_PATH/intermediate/service models/ms2.xmi" \
     -t "$TARGET_PATH""/ms2" \
     --invoke_only_specified_phases \
     code_generation \
