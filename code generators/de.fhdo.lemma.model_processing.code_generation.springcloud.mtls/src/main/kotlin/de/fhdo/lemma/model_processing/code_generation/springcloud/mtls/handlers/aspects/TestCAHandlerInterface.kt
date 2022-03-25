350067f6ac66520752ac20719cee5c0c836b1f84
Dominik Pelkmann <dominik@pelkmann.com>
2022-03-25 18:01:04 +0100

    wip

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/mtls/handlers/aspects/TestCAHandlerInterface.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.mtls/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/mtls/handlers/aspects/TestCAHandlerInterface.kt
@@ -2,7 +2,6 @@ package de.fhdo.lemma.model_processing.code_generation.springcloud.mtls.handlers
 
 import com.github.javaparser.ast.Modifier
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
-import de.fhdo.lemma.data.intermediate.IntermediateImportedAspect
 import de.fhdo.lemma.model_processing.code_generation.java_base.ast.ImportTargetElementType
 import de.fhdo.lemma.model_processing.code_generation.java_base.ast.SerializationCharacteristic
 import de.fhdo.lemma.model_processing.code_generation.java_base.ast.addAndGetAnnotation
@@ -10,31 +9,29 @@ import de.fhdo.lemma.model_processing.code_generation.java_base.ast.addAnnotatio
 import de.fhdo.lemma.model_processing.code_generation.java_base.ast.addDependency
 import de.fhdo.lemma.model_processing.code_generation.java_base.ast.addImport
 import de.fhdo.lemma.model_processing.code_generation.java_base.ast.addSerializationCharacteristic
+import de.fhdo.lemma.model_processing.code_generation.java_base.ast.getPackageName
+import de.fhdo.lemma.model_processing.code_generation.java_base.ast.newJavaClassOrInterface
 import de.fhdo.lemma.model_processing.code_generation.java_base.ast.setBody
 import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.GenletCodeGenerationHandlerI
 import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.GenletCodeGenerationHandlerResult
 import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.GenletGeneratedFileContent
+import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.GenletGeneratedNode
 import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.GenletPathSpecifier
 import de.fhdo.lemma.model_processing.code_generation.java_base.getAllAspects
 import de.fhdo.lemma.model_processing.code_generation.java_base.getAspectPropertyValue
 import de.fhdo.lemma.model_processing.code_generation.java_base.handlers.CodeGenerationHandler
 import de.fhdo.lemma.model_processing.code_generation.java_base.hasApiComments
 import de.fhdo.lemma.model_processing.code_generation.java_base.hasAspect
-import de.fhdo.lemma.model_processing.code_generation.java_base.qualifiedName
 import de.fhdo.lemma.model_processing.code_generation.java_base.serialization.property_files.PropertyFile
 import de.fhdo.lemma.model_processing.code_generation.java_base.serialization.property_files.openPropertyFile
-import de.fhdo.lemma.model_processing.code_generation.springcloud.mtls.Context
 import de.fhdo.lemma.service.intermediate.IntermediateMicroservice
-import de.fhdo.lemma.model_processing.code_generation.springcloud.mtls.Context.State as State
-import org.eclipse.emf.ecore.EObject
-import org.eclipse.equinox.internal.app.Messages
+import java.io.File
 
 @CodeGenerationHandler
 internal class TestCAHandlerInterface
     : GenletCodeGenerationHandlerI<IntermediateMicroservice,ClassOrInterfaceDeclaration, Nothing>{
     override fun handlesEObjectsOfInstance() = IntermediateMicroservice::class.java
     override fun generatesNodesOfInstance() = ClassOrInterfaceDeclaration::class.java
-
     override fun execute(
         eObject: IntermediateMicroservice,
         node: ClassOrInterfaceDeclaration,
@@ -43,10 +40,10 @@ internal class TestCAHandlerInterface
         if (!eObject.hasAspect("mTLS.Keystore", "mTLS.TestKeystore"))
             return GenletCodeGenerationHandlerResult(node)
         var currentApplicationPropertiesFile = openPropertyFile(GenletPathSpecifier.CURRENT_MICROSERVICE_RESOURCES_PATH, "application-dev.properties")
-        var currentApplicationPropertiesFile1 = openPropertyFile(GenletPathSpecifier.CURRENT_DOMAIN_GENERATION_TARGET_PATH, "application-dev.properties")
-        var currentApplicationPropertiesFile2 = openPropertyFile(GenletPathSpecifier.CURRENT_MICROSERVICE_GENERATION_TARGET_PATH, "application-dev.properties")
-        var currentApplicationPropertiesFile3 = openPropertyFile(GenletPathSpecifier.CURRENT_INTERFACE_GENERATION_TARGET_PATH, "application-dev.properties")
-        var currentApplicationPropertiesFile4 = openPropertyFile(GenletPathSpecifier.CURRENT_MICROSERVICE_JAVA_ROOT_PATH, "application-dev.properties")
+        var currentApplicationPropertiesFile1 = openPropertyFile(GenletPathSpecifier.CURRENT_MICROSERVICE_RESOURCES_PATH, "application1-dev.properties")
+        var currentApplicationPropertiesFile2 = openPropertyFile(GenletPathSpecifier.CURRENT_MICROSERVICE_RESOURCES_PATH, "application2-dev.properties")
+        var currentApplicationPropertiesFile3 = openPropertyFile(GenletPathSpecifier.CURRENT_MICROSERVICE_RESOURCES_PATH, "application3-dev.properties")
+        var currentApplicationPropertiesFile4 = openPropertyFile(GenletPathSpecifier.CURRENT_MICROSERVICE_RESOURCES_PATH, "application-dev4.properties")
 
 
         println("GenletPathSpecifier.CURRENT_MICROSERVICE_RESOURCES_PATH ${currentApplicationPropertiesFile.filePath}" )
@@ -67,10 +64,10 @@ internal class TestCAHandlerInterface
             SerializationCharacteristic.DONT_RELOCATE)
         node.addImport("org.springframework.context.annotation.Profile",
             ImportTargetElementType.ANNOTATION, SerializationCharacteristic.DONT_RELOCATE)
-        node.addImport("import javax.net.ssl.HttpsURLConnection",
+        node.addImport("javax.net.ssl.HttpsURLConnection",
             ImportTargetElementType.ANNOTATION, SerializationCharacteristic.DONT_RELOCATE)
         // Add main method to invoke the SpringBoot application
-        val currentMicroservicePackage: String by state()
+
         node.addAndGetAnnotation("Configuration", SerializationCharacteristic.DONT_RELOCATE)
         node.addAndGetAnnotation("Configuration", SerializationCharacteristic.DONT_RELOCATE)
 
@@ -106,9 +103,17 @@ internal class TestCAHandlerInterface
         println(currentApplicationPropertiesFile.filePath)
         currentApplicationPropertiesFile["spring.test"] = "huhu1"
 
+        currentApplicationPropertiesFile1["spring.test1"] = "File1"
+        currentApplicationPropertiesFile2["spring.test2"] = "File2"
+        currentApplicationPropertiesFile3["spring.test3"] = "File3"
+        currentApplicationPropertiesFile4["spring.test4"] = "File4"
+
         currentApplicationPropertiesFile.forEach { key, value -> println("DEFAULT key ${key} value ${value}") }
 
-        return GenletCodeGenerationHandlerResult(node, generatedApplicationPropertiesFile(currentApplicationPropertiesFile))
+        return GenletCodeGenerationHandlerResult(node,
+            mutableSetOf(GenletGeneratedFileContent(currentApplicationPropertiesFile),
+                generatBypassSpringConfigutationFile(node.getPackageName(), "mTLSBypassConfiguration"))
+            )
     }
 
 
@@ -119,4 +124,31 @@ internal class TestCAHandlerInterface
     private fun generatedApplicationPropertiesFile(currentApplicationPropertiesFile: PropertyFile) : MutableSet<GenletGeneratedFileContent> {
         return mutableSetOf(GenletGeneratedFileContent(currentApplicationPropertiesFile))
     }
+
+    private fun generatBypassSpringConfigutationFile(packageName: String, className: String): GenletGeneratedFileContent {
+
+        val configFolder = "configuration"
+
+        val node = newJavaClassOrInterface("${packageName}.${configFolder}", className, isInterface = false)
+        node.addImport("org.springframework.boot.autoconfigure.condition.ConditionalOnExpression",
+            ImportTargetElementType.ANNOTATION
+        )
+        node.addImport("org.springframework.context.annotation.Configuration", ImportTargetElementType.ANNOTATION)
+        node.addImport("javax.net.ssl.HttpsURLConnection", ImportTargetElementType.METHOD_BODY, )
+
+        node.addAndGetAnnotation("Configuration")
+        node.addAndGetAnnotation("ConditionalOnExpression")
+            .addPair("value", "\"\${server.ssl.bypass.hostname-verifier:true}\"")
+
+        node.addConstructor(Modifier.Keyword.PUBLIC)
+            .body.addStatement("HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);")
+
+
+        return GenletGeneratedFileContent(
+            GenletPathSpecifier.CURRENT_MICROSERVICE_GENERATION_TARGET_PATH,
+            "${configFolder}${File.separator}${node.nameAsString}.java",
+            node
+        )
+    }
+
 }
