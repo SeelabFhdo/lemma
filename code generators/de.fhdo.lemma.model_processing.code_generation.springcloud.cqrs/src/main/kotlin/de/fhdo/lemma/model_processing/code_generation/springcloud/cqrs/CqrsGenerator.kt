195dd8d1b3b673b6e653d5ac487603d89a9ae4c6
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2022-04-10 19:39:31 +0200

    CQRS Genlet: Harmonize LEMMA technology names of aspects

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.cqrs/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/cqrs/CqrsGenerator.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.cqrs/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/cqrs/CqrsGenerator.kt
@@ -6,6 +6,13 @@ import de.fhdo.lemma.model_processing.code_generation.java_base.genlets.Abstract
 private const val BASE_PACKAGE = "de.fhdo.lemma.model_processing.code_generation.springcloud.cqrs"
 
 /**
+ * Supported names for the CQRS technology.
+ *
+ * @author [Florian Rademacher](mailto:florian.rademacher@fh-dortmund.de)
+ */
+internal val CQRS_TECHNOLOGY_NAMES = setOf("cqrs", "CQRS")
+
+/**
  * Main class of the CQRS generator realized on the basis of LEMMA's model processing framework as an
  * [AbstractModelProcessor].
  *
