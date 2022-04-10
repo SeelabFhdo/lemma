148b070612e69933a16209d4130291da9aaa70da
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2022-04-10 19:37:43 +0200

    Domain Events Genlet: Harmonize LEMMA technology names of aspects

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.domain_events/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/domain_events/Util.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.domain_events/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/domain_events/Util.kt
@@ -1,5 +1,6 @@
 package de.fhdo.lemma.model_processing.code_generation.springcloud.domain_events
 
+import de.fhdo.lemma.model_processing.code_generation.java_base.forTechnologies
 import de.fhdo.lemma.model_processing.utils.findAliasForTechnology
 import de.fhdo.lemma.service.Microservice
 import de.fhdo.lemma.technology.mapping.ComplexTypeMapping
@@ -10,7 +11,7 @@ import de.fhdo.lemma.technology.mapping.ComplexTypeMapping
  * @author [Florian Rademacher](mailto:florian.rademacher@fh-dortmund.de)
  */
 internal val Microservice.domainEventsAlias
-    get() = technologyReferences.findAliasForTechnology("DomainEvents")
+    get() = technologyReferences.findAliasForTechnology(DOMAIN_EVENTS_TECHNOLOGY_NAMES)
 
 /**
  * Get alias of Domain Events technology model from [ComplexTypeMapping].
@@ -18,4 +19,13 @@ internal val Microservice.domainEventsAlias
  * @author [Florian Rademacher](mailto:florian.rademacher@fh-dortmund.de)
  */
 internal val ComplexTypeMapping.domainEventsAlias
-    get() = technologyReferences.findAliasForTechnology("DomainEvents")
+    get() = technologyReferences.findAliasForTechnology(DOMAIN_EVENTS_TECHNOLOGY_NAMES)
+
+/**
+ * Build the [Set] of qualified aspect names for the DomainEvents technology from this [String] representing a simple
+ * aspect name. For instance, for the simple name "EventGroup" this function returns the [Set]
+ * {"DomainEvents.EventGroup", "domainEvents.EventGroup"}.
+ *
+ * @author [Florian Rademacher](mailto:florian.rademacher@fh-dortmund.de)
+ */
+fun String.forDomainEventsTechnology() = forTechnologies(DOMAIN_EVENTS_TECHNOLOGY_NAMES)
