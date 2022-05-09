23ead9f1003637ca42e591d90b793b170a6e0010
Dominik Pelkmann <dominik@pelkmann.com>
2022-05-09 20:58:49 +0200

    wip

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.keycloak/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/keycloak/Context.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud.keycloak/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/keycloak/Context.kt
@@ -1,29 +1,13 @@
-package de.fhdo.lemma.model_processing.code_generation.springcloud.mtls
-
-data class Properties(val filename: String, val fileType: FileType, val propertiesSet: MutableSet<Pair<String, String>>)
+package de.fhdo.lemma.model_processing.code_generation.springcloud.keycloak
 
 internal object Context {
 
     object State {
-        private val propertyFiles = mutableMapOf<String, Properties>()
-
         fun initialize() {
-            propertyFiles.clear()
         }
 
         fun reset() {
-            propertyFiles.clear()
         }
-
-        fun addPropertiesToFile(fileName: String, propertiesSet: Set<Pair<String, String>>, fileType: FileType) {
-            val properties = Properties(fileName, fileType, mutableSetOf())
-            propertiesSet.forEach { property ->
-                properties.propertiesSet.add(property)
-            }
-            propertyFiles[fileName] = properties
-        }
-
-        fun getPropertyFiles() = propertyFiles
     }
 }
 
