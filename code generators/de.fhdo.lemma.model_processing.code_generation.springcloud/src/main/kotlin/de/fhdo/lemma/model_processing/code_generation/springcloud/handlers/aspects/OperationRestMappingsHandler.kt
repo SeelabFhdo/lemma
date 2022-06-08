771bd6d8f8075fd5fa9ca30611f2b191f25e3e28
Dominik Pelkmann <dominik@pelkmann.com>
2022-06-09 00:01:19 +0200

    Revert "Spring Cloud Genlet: Support aspects with eponymous Spring annotation names in technology models with "Spring" qualifier"
    This reverts commit 5e4f81e31ff96220750ff6882c2c3e380b2fe1b2.

--- code generators/de.fhdo.lemma.model_processing.code_generation.springcloud/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/handlers/aspects/OperationRestMappingsHandler.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.springcloud/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/springcloud/handlers/aspects/OperationRestMappingsHandler.kt
@@ -36,13 +36,7 @@ internal class OperationRestMappingsHandler : AspectHandlerI {
         "Spring.Put" to "PutMapping",
         "Spring.Post" to "PostMapping",
         "Spring.Delete" to "DeleteMapping",
-        "Spring.Patch" to "PatchMapping",
-
-        "Spring.GetMapping" to "GetMapping",
-        "Spring.PutMapping" to "PutMapping",
-        "Spring.PostMapping" to "PostMapping",
-        "Spring.DeleteMapping" to "DeleteMapping",
-        "Spring.PatchMapping" to "PatchMapping"
+        "Spring.Patch" to "PatchMapping"
     )
 
     override fun handlesAspects() : Set<String> {
