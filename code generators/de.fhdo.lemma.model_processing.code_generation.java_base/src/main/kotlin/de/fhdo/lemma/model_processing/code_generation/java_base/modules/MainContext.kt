7a1ad6f12c2f24cad6ef965669d04d0d1228d100
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2022-08-08 10:03:46 +0200

    Java Base Generator: Access targetFolderPath attribute directly instead of encapsulating it in a seperate private method

--- code generators/de.fhdo.lemma.model_processing.code_generation.java_base/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/java_base/modules/MainContext.kt
+++ code generators/de.fhdo.lemma.model_processing.code_generation.java_base/src/main/kotlin/de/fhdo/lemma/model_processing/code_generation/java_base/modules/MainContext.kt
@@ -144,7 +144,7 @@ internal object MainContext {
                 "aspectHandlers" -> aspectHandlers.toMap()
                 "collectedDependencies" -> collectedDependencies.toSet()
                 "currentMicroservicePackage" -> currentMicroservicePackage
-                "currentMicroserviceTargetFolderPathRootPath" -> currentMicroserviceTargetFolderPathRootPath()
+                "currentMicroserviceTargetFolderPathRootPath" -> targetFolderPath
                 "currentMicroserviceTargetFolderPath" -> currentMicroserviceTargetFolderPath()
                 "currentMicroserviceTargetFolderPathForJavaFiles" -> currentMicroserviceTargetFolderPathForJavaFiles()
                 "currentMicroserviceTargetFolderPathForResourceFiles" ->
@@ -166,13 +166,6 @@ internal object MainContext {
             }
 
             return value as T
-        }
-
-        /**
-         * Helper to calculate the target root folder path of the state's [currentMicroservicePackage]
-         */
-        private fun currentMicroserviceTargetFolderPathRootPath() : String {
-            return targetFolderPath
         }
 
         /**
